c3b1f1f0cf86170e3686b3e096bd909c260669d4
intaen <caltea.kid@gmail.com>
2021-09-18 19:12:10 +0700

    [UPDATE] change constant pos in util string

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	data := "PPKM diperpanjang hingga 2021-10-07"
-	date := str.GetStr(data, 1, 10)     // Get last string
+	date := str.GetStr(data, -1, 10)     // Get last string
 	month := inamonth.ConvertDate(date) // Convert date to date with name of month
 
 	fmt.Println(strings.Replace(data, date, month, -1)) // Replace data
